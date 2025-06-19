8ec304462191338a9a1a80a0348bf97131f53abd
jcdiv47 <jackchai2.0@gmail.com>
2025-06-19 18:00:14 +0800

    Update config

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -3,3 +3,5 @@ local set = vim.opt_local
 set.textwidth = 90
 set.wrap = true
 set.linebreak = true
+
+vim.keymap.set('n', '<leader>m', '<cmd>Markview toggle<CR>', { buffer = true, desc = 'Markview Toggle' })
